0c6d80743a046194df24a2a4c704c2e72a028694
osamudiamen <osas.azamegbe@gmail.com>
2020-11-05 14:35:44 +0100

    FEAT: Created the Navbar component with Routes and Router.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React from 'react';
 import './App.css';
 
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
 import Home from './components/pages/home/Home'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </Router>
     </div>
   );
 }
