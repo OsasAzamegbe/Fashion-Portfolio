3ff4c9eb60c296ebb0a3b2d76f20315edda102c5
osamudiamen <osas.azamegbe@gmail.com>
2020-11-06 19:53:49 +0100

    CHORE: modified images.

--- src/components/Image.js
+++ src/components/Image.js
@@ -5,7 +5,7 @@ import './Image.css'
 const Image = ({src, alt}) => {
     return (
         <div className="image-container">
-            <img src={src} alt={alt} className="image"/>
+            <img src={src} alt={alt} loading="lazy" className="image"/>
         </div>
     )
 }
--- src/components/pages/illustrations/Data.js
+++ src/components/pages/illustrations/Data.js
@@ -3,7 +3,7 @@ const IMGPATH = '../../../images/illustrations'
 
 const imageObjs = [
     {
-        src: `${IMGPATH}/image1.png`
+        src: `https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/122792886_2729462300666861_1736517696981542663_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net\u0026_nc_cat=105\u0026_nc_ohc=1_m_72bfyMUAX8cWBFv\u0026_nc_tp=24\u0026oh=d622c56f264203a091d6aad73e99f256\u0026oe=5FCEA170`
     },
     {
         src: `${IMGPATH}/image2.png`
