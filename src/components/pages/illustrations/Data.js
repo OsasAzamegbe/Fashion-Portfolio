8ea4f34e531002c83e1110a46028a727c5ac2cca
osamudiamen <osas.azamegbe@gmail.com>
2020-11-06 20:02:23 +0100

    FEAT: Added image src data

--- src/components/pages/illustrations/Data.js
+++ src/components/pages/illustrations/Data.js
@@ -3,22 +3,22 @@ const IMGPATH = '../../../images/illustrations'
 
 const imageObjs = [
     {
-        src: `https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/p640x640/122792886_2729462300666861_1736517696981542663_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net\u0026_nc_cat=105\u0026_nc_ohc=1_m_72bfyMUAX8cWBFv\u0026_nc_tp=24\u0026oh=d622c56f264203a091d6aad73e99f256\u0026oe=5FCEA170`
+        src: "https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/122792886_2729462300666861_1736517696981542663_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net\u0026_nc_cat=105\u0026_nc_ohc=1_m_72bfyMUAX8cWBFv\u0026tp=18\u0026oh=f52ed741b026d33d8ca548cabfe3f247\u0026oe=5FD07C38"
     },
     {
-        src: `${IMGPATH}/image2.png`
+        src: "https://scontent-los2-1.cdninstagram.com/v/t51.2885-15/e35/120083731_131902514937011_7267434626559291401_n.jpg?_nc_ht=scontent-los2-1.cdninstagram.com\u0026_nc_cat=111\u0026_nc_ohc=U-IvXYm_uUEAX9u9Gk9\u0026tp=18\u0026oh=14e3efdd9e712ed6f6cce1307f9afedc\u0026oe=5FD0F753"
     },
     {
-        src: `${IMGPATH}/image3.png`
+        src: "https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/120721589_136432631504799_3891571474760017877_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net\u0026_nc_cat=103\u0026_nc_ohc=Chtcf_sWhI4AX9qzRkH\u0026tp=18\u0026oh=16776330621009f7c250f5604ac572a1\u0026oe=5FCDEFB0"
     },
     {
-        src: `${IMGPATH}/image4.png`
+        src: "https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/119887928_1731565226995782_2114781389310937958_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net\u0026_nc_cat=106\u0026_nc_ohc=vWsERX5H--UAX8T5eiN\u0026tp=18\u0026oh=a67d5bfaa403ba810735e8935134610a\u0026oe=5FD073F4"
     },
     {
-        src: `${IMGPATH}/image5.png`
+        src: "https://scontent-los2-1.cdninstagram.com/v/t51.2885-15/e35/120453237_2388975891246046_3175559045737406791_n.jpg?_nc_ht=scontent-los2-1.cdninstagram.com\u0026_nc_cat=110\u0026_nc_ohc=-jXrcmtNloMAX-e4WaT\u0026tp=18\u0026oh=11c012acbfb86409f299d7f4cfb45621\u0026oe=5FCE3FB1"
     },
     {
-        src: `${IMGPATH}/image6.png`
+        src: "https://instagram.flos1-1.fna.fbcdn.net/v/t51.2885-15/e35/121587446_387807745955905_7150622681795755524_n.jpg?_nc_ht=instagram.flos1-1.fna.fbcdn.net\u0026_nc_cat=101\u0026_nc_ohc=819oGZCP-SQAX-mqlNV\u0026tp=18\u0026oh=129bd104f9816fbfc2b2d2e8670a07af\u0026oe=5FCF551F"
     }
 ] 
 
