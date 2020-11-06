be8f1a5210706c36bc383de1f1bda603f1b16f04
osamudiamen <osas.azamegbe@gmail.com>
2020-11-06 19:26:36 +0100

    FEAT: Created Image Data. Mounted Images to Illustrations component.

---
+++ src/components/pages/illustrations/Data.js
@@ -0,0 +1,25 @@
+const IMGPATH = '../../../images/illustrations'
+
+
+const imageObjs = [
+    {
+        src: `${IMGPATH}/image1.png`
+    },
+    {
+        src: `${IMGPATH}/image2.png`
+    },
+    {
+        src: `${IMGPATH}/image3.png`
+    },
+    {
+        src: `${IMGPATH}/image4.png`
+    },
+    {
+        src: `${IMGPATH}/image5.png`
+    },
+    {
+        src: `${IMGPATH}/image6.png`
+    }
+] 
+
+module.exports = {imageObjs}
--- src/components/pages/illustrations/Illustrations.js
+++ src/components/pages/illustrations/Illustrations.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './Illustrations.css'
 
+import {imageObjs} from './Data'
+import Image from '../../Image'
+
 
 const Illustrations = () => {
     return(
         <div className="illustrations">
             <h1>PORTFOLIO</h1>
+            {imageObjs.map(
+                (image, index) => <Image {...image} key={index}/>
+            )}
         </div>
     )
 }
