4b811875ad636e4177334a17a1494477b74fde6e
osamudiamen <osas.azamegbe@gmail.com>
2020-11-07 19:12:28 +0100

    FEAT: added about text

--- src/components/pages/about/About.js
+++ src/components/pages/about/About.js
@@ -5,7 +5,16 @@ import './About.css'
 const About = () => {
     return (
         <div className="about">
-            <p>About Me</p>
+            <div className="about-description-container">
+                <p className="about-headliner">
+                    Mobola Andé is a Nigerian self-taught womenswear fashion illustrator based in Lagos, Nigeria.
+                </p>
+                <p className="about-text">
+                    <br/>
+                    She thinks of fashion and design as her alter ego. Mobola draws inspiration for her fashion illustration from within. Her designs are based on her ever dynamic fashion tastes and subtle ingenuity with colors. Mobola's style is comforting, ingenious and expressive. 
+                    <br/><br/>Mobola's ultimate goal as an artist is to produce high quality wears that keep challenging design possibilities, pioneer new frontiers in Fashion whilst commandeering timeless aesthetics.
+                </p>
+            </div>            
         </div>
     )
 }
